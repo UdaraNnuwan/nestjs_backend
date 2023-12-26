a18f0ebf718c5fe0e3b1e7aa2bff954d73b81244
Udara Nuwan <unkothalawala@gmail.com>
2023-12-26 19:33:15 +0530

    Connect with SQL data base using typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './typeorm';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [CustomersModule, UsersModule,TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'localhost',
+    username:'admin',
+    password:'admin1234',
+    database:'nestjs_db',
+    entities:entities,
+    synchronize:true,
+  })],
   controllers: [],
   providers: [],
 })
