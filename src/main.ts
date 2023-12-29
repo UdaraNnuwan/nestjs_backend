1ab0fce421fdfd7d6cdbec36c92c7ebc180ae3f1
Udara Nuwan <unkothalawala@gmail.com>
2023-12-29 14:20:42 +0530

    session create using cookie

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
+  app.use(session({
+    name:"Nest_js_session_id",
+    secret:"UdaeaSesstion",
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+      maxAge:60000,
+    },
+  }))
   await app.listen(5002);
 }
 bootstrap();
